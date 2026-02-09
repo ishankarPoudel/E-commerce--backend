6aa575ee8291374521be817a265a900a738ce37e
Shankar <shankarpoudel499@gmail.com>
2026-02-09 13:56:03 -0400

    fix: datasource cconfig

--- src/config/data-source/data-source.ts
+++ src/config/data-source/data-source.ts
@@ -1,13 +1,12 @@
-import { join } from "path";
 import { DataSource } from "typeorm";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: process.env.DATABASE_HOSTNAME || "localhost",
   port: 5432,
-  username: "postgres",
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME || "postgres",
+  password: process.env.DATABASE_PASSWORD || "classmate",
+  database: process.env.DATABASE_NAME || "ecommerce",
   synchronize: true,
   logging: false,
   entities: ["src/entities/**/*.ts"],
