55732815027aa5afbe346db8c13ddb6735223661
Shankar <shankarpoudel499@gmail.com>
2026-02-10 00:27:14 -0400

    fix: nnodemailer tconnectin timeouts, and other configs

--- src/config/mail/mail.config.ts
+++ src/config/mail/mail.config.ts
@@ -2,12 +2,17 @@ import nodemailer from "nodemailer";
 
 export const mailTransport = nodemailer.createTransport({
   service: "gmail",
-  // host: "smtp.gmail.com",
-  // port: 587,
+  host: "smtp.gmail.com",
+  port: 587,
   secure: false, // true for 465, false for other ports
   requireTLS: false, // true if you want to use TLS
   auth: {
     user: process.env.GMAIL_USER || "shankarpoudel499@gmail.com",
     pass: process.env.GMAIL_PASS || "lfxw munn symz nzpd",
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000, // 10 seconds
 });
